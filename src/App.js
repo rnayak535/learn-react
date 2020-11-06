3ae48e3b721476b41f9736a7fe6d03224ac45a55
Rajesh <rnayak535@gmail.com>
2020-11-06 22:13:52 +0530

    using state prop

--- src/App.js
+++ src/App.js
@@ -3,10 +3,29 @@ import './App.css';
 import Todos from './components/Todos';
 
 class App extends Component {
+  state = {
+    todos: [
+        {
+            id: 1,
+            title: 'Clean up Bed',
+            completed: false
+        },
+        {
+            id: 2,
+            title: 'Go to Bed',
+            completed: false
+        },
+        {
+            id: 3,
+            title: 'Dont keep mobile side to your head',
+            completed: false
+        }
+    ]
+}
   render() {
       return (
       <div className="App">
-        <Todos />
+        <Todos todos={this.state.todos}/>
       </div>
     );
   }
